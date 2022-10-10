d219708b6d42444aa42766f35372d12feb1d5c6f
DevCandido <dev.kelvynjohnson@gmail.com>
2022-10-10 18:06:02 -0300

    first commit

---
+++ convert_temperatura.py
@@ -0,0 +1,4 @@
+temperatura_f = float(input('Digite a temperatura em Fahrenheit: '))
+temperatura_c = round (5*((temperatura_f - 32) / 9), 2)
+print()
+print(f'A temperatura em Celsius é: {temperatura_c}')
